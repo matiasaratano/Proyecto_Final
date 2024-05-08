5554af30dacb7a965e1f53e6b90ee5086a75c32f
Kevin Mercs <kevin.mercs.kam@gmail.com>
2024-05-08 19:43:35 -0300

    Subo el ReservaController

--- API/controllers/ReservaController.js
+++ API/controllers/ReservaController.js
@@ -7,13 +7,32 @@ const formateador = new Formateador
 class ReservaController {
     constructor() { }
 
-    createReserva = async (req,res) => {
+    createReserva = async (req, res) => {
         try {
-            res.status(200).send({ success: true, message: "User" });
+            const { fecha, vianda, userId } = req.body;
+            
+            // Verificamso la disponibilidad de asientos en la oficina
+            const reservasExistente = await Reserva.count();
+            if (reservasExistente >= 24) {
+                return res.status(400).send({ success: false, message: 'No hay asientos disponibles para realizar la reserva.' });
+            }
 
-        }
-        catch (error) {
-            res.status(400).send({ success: false, message: error.message })
+            // Creamos reserva si hay espacio
+            const reserva = await Reserva.create({
+                fecha,
+                vianda,
+                UserId: userId
+            });
+
+            // Actualizamos disponibilidad de asientos
+            const asientosDisponibles = 24 - reservasExistente - 1; // Restamos 1 por la reserva actual
+            console.log(`Quedan ${asientosDisponibles} asientos disponibles.`);
+
+            // Se notifica el estado del cierre de la ejecucion
+            res.status(200).send({ success: true, message: 'Reserva realizada satisfactoriamente.' });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ success: false, message: 'Error al crear la reserva.' });
         }
     }
 
@@ -92,16 +111,26 @@ class ReservaController {
 
 
 
-    deleteReserva = async (req,res) => {
+    deleteReserva = async (req, res) => {
         try {
-            res.status(200).send({ success: true, message: "User" });
-
-        }
-        catch (error) {
-            res.status(400).send({ success: false, message: error.message })
+            const { reservaId } = req.params; // Obtenemos el ID de la reserva de los parámetros de la solicitud
+            
+            // Verificamos si la reserva existe
+            const reserva = await Reserva.findByPk(reservaId);
+            if (!reserva) {
+                return res.status(404).send({ success: false, message: 'Reserva no encontrada.' });
+            }
+    
+            // Eliminamos la reserva
+            await reserva.destroy();
+    
+            // Enviamos una respuesta de éxito
+            res.status(200).send({ success: true, message: 'Reserva eliminada correctamente.' });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ success: false, message: 'Error al eliminar la reserva.' });
         }
     }
-
 }
 
 export default ReservaController
