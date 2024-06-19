6fea5bcab3f1e3e58e40aeb891aa5cc93b6d80a9
Ezequiel Venuti <129341756+EzequielVenuti@users.noreply.github.com>
2024-06-19 12:00:14 -0300

    se agrega cors

--- API/index.js
+++ API/index.js
@@ -2,8 +2,11 @@ import 'dotenv/config';
 import express from "express"
 import router from "./routes/router.js";
 import connection from "./connection/connection.js";
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
