253cf4c26a0b6a21b88fbfd21214b3c4dd262043
Matias <matiasaratano@gmail.com>
2024-06-23 21:49:46 +0200

    modificacion screen envioContrasena

--- screens/Login/envioContraseña.js
+++ screens/Login/envioContraseña.js
@@ -1,63 +1,28 @@
 import React, { useState } from 'react';
 import { View, Alert } from 'react-native';
 import { Button } from 'react-native-paper';
-import CustomInput from '../../components/CustomInput';
+// import CustomInput from '../../components/CustomInput';
 import TextoEncerrado from '../../components/TextoEncerrado';
 
 import styles from '../../styles/styles';
 import buttons from '../../styles/buttons';
 
 export default ({ navigation }) => {
-  const [email, setEmail] = useState('');
+  // const [email, setEmail] = useState('');
 
   const handleVolver = () => {
     navigation.navigate('Login');
   };
 
-  const handleEnviar = () => {
-    fetch('URL DEL ENDPOINT DE LA API', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.success) {
-          Alert.alert(
-            'Email enviado',
-            'Por favor, revisa tu correo electrónico para obtener tu contraseña.'
-          );
-          navigation.navigate('Login');
-        } else {
-          Alert.alert(
-            'Error',
-            'Hubo un problema al enviar el correo electrónico.'
-          );
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  };
-
   return (
     <View style={styles.container}>
       <TextoEncerrado
-        text="Por favor, introduce tu dirección de correo electrónico alternativo o personal a continuación para que podamos enviarte la información de inicio de sesión."
+        text="Por favor, comunicate con RRHH para que puedan gestionarte una nueva clave para iniciar sesión."
         fontSize={18}
       />
       <View style={{ height: 20 }} />
-      <CustomInput
-        label="Correo electrónico"
-        value={email}
-        onChangeText={setEmail}
-      />
+
       <View style={buttons.containerbutton}>
-        <Button mode="contained" style={buttons.button} onPress={handleEnviar}>
-          Enviar
-        </Button>
         <Button
           mode="contained-tonal"
           style={buttons.button}
